d3b2758b63fb2e7a1c60fb7f855f011ebe10bf44
Nathan Dion Schmid <nathandion.schmid@gmail.com>
2025-11-12 16:58:36 +0100

    Add USER_PASSWORD field to USERS table and update insert statements

--- database/users.sql
+++ database/users.sql
@@ -1,14 +1,15 @@
 /*
 
-+────────┌────────┌──────────┌─────────────┌────────────────┌──────────┌───────────────┌─────────────────+
-|USER_ID |COMP_ID |USER_ABBR |USER_SURNAME |USER_FIRST_NAME |USER_ROLE |USER_JOIN_DATE |USER_UPDATE_DATE |
-+────────└────────└──────────└─────────────└────────────────└──────────└───────────────└─────────────────+
++────────┌────────┌──────────┌─────────────┌────────────────┌──────────┌──────────────┌───────────────┌─────────────────+
+|USER_ID |COMP_ID |USER_ABBR |USER_SURNAME |USER_FIRST_NAME |USER_ROLE |USER_PASSWORD |USER_JOIN_DATE |USER_UPDATE_DATE |
++────────└────────└──────────└─────────────└────────────────└──────────└──────────────└───────────────└─────────────────+
 USER_ID             INT             AUTO_INCREMENT                          PRIMARY KEY,
 COMP_ID             INT             FOREIGN KEY REFERENCES COMPANY(COMP_ID),
 USER_ABBR           VARCHAR(5)      NOT NULL,
 USER_SURNAME        VARCHAR(100)    NOT NULL,
-USER_ROLE           VARCHAR(50)     NOT NULL,
 USER_FIRST_NAME     VARCHAR(100)    NOT NULL,
+USER_ROLE           VARCHAR(50)     NOT NULL,
+USER_PASSWORD       VARCHAR(255)    NOT NULL,
 USER_JOIN_DATE      DATETIME        DEFAULT CURRENT_TIMESTAMP
 USER_UPDATE_DATE    DATETIME        DEFAULT NULL                            ON UPDATE CURRENT_TIMESTAMP
 
@@ -21,6 +22,7 @@ CREATE TABLE USERS (
     USER_SURNAME        VARCHAR(100)    NOT NULL,
     USER_FIRST_NAME     VARCHAR(100)    NOT NULL,
     USER_ROLE           VARCHAR(50)     NOT NULL,
+    USER_PASSWORD       VARCHAR(255)    NOT NULL,
     USER_JOIN_DATE      DATETIME        DEFAULT CURRENT_TIMESTAMP,
     USER_UPDATE_DATE    DATETIME        DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (COMP_ID) REFERENCES COMPANY(COMP_ID)
